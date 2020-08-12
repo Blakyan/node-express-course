ccfb817a2a161a5c03f6565a54777a167793b086
melykan <galaxytabkarl@gmail.com>
2020-08-12 05:39:07 +0100

    add login POST route

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const express = require('express');
 
 // create an instance of the express consntructor which will be name app
 const app = express(); 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 //The server will return somme mock data just for us to test it
 const mockUserData=[
     { name: 'Mark' },
     { name: 'Jill' }
 ]
-
+// GET REQUEST
 app.get('/users/:id',function(req,res){
     console.log(req.params.id)
     res.json({
@@ -18,6 +20,29 @@ app.get('/users/:id',function(req,res){
         user: req.params.id
     })
 })
+
+//POST REQUEST
+app.post('/login',function(req,res){
+    const username= req.body.username;
+    const password= req.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="SuperSecret";
+
+    if (username===mockUsername && password===mockPassword){
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        })
+    }else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
+})
+
 
 app.listen(8000,function(){
 
